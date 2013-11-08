232f8e6c9a929ae8962cb41c2b2cdc7f0233b761
Esteba Arango <marranoparael31@gmail.com>
2013-11-07 23:59:17 -0500

    Current location working

--- grafmap-project/web/assets/js/main.js
+++ grafmap-project/web/assets/js/main.js
@@ -1,5 +1,44 @@
 (function() {
-  console.log('Testing');
+  var error, success;
+
+  success = function(position) {
+    var latlng, map, marker, myOptions, s;
+    s = document.querySelector("#status");
+    if (s.className === "success") {
+      return;
+    }
+    s.innerHTML = "found you!";
+    s.className = "success";
+    latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    myOptions = {
+      zoom: 15,
+      center: latlng,
+      mapTypeControl: false,
+      navigationControlOptions: {
+        style: google.maps.NavigationControlStyle.SMALL
+      },
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById("map"), myOptions);
+    return marker = new google.maps.Marker({
+      position: latlng,
+      map: map,
+      title: "You are here! (at least within a " + position.coords.accuracy + " meter radius)"
+    });
+  };
+
+  error = function(msg) {
+    var s;
+    s = document.querySelector("#status");
+    s.innerHTML = (typeof msg === "string" ? msg : "failed");
+    return s.className = "fail";
+  };
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, error);
+  } else {
+    alert("Oops, your browser doesn't support geo-location.");
+  }
 
 }).call(this);
 
