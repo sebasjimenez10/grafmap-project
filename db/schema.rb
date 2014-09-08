4c3bb75746fefb687f823afecb99c9709f93b8dd
Sebastian Jimenez <sebasjimenezv@gmail.com>
2014-09-07 19:04:11 -0500

    updating gems

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140904023941) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "favorite_places", force: true do |t|
     t.string   "user_id"
     t.string   "category"
