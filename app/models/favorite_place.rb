aeb42315009fe43d4e34d47555cb35701d07e81e
Esteban Arango Medina <marranoparael31@gmail.com>
2014-10-14 22:02:12 -0500

    Annotating and removing files.

--- app/models/favorite_place.rb
+++ app/models/favorite_place.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: favorite_places
+#
+#  id         :integer          not null, primary key
+#  user_id    :string(255)
+#  category   :string(255)
+#  latitud    :string(255)
+#  longitud   :string(255)
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class FavoritePlace < ActiveRecord::Base
 end
